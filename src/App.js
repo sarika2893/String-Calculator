5b5c8ad3abbbc923e2fca609ef3ea72800a4019c
sarika <sarikachavan2893@gmail.com>
2024-10-04 02:06:17 +0530

    added firebase for the deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import NewCompo from "./components/NewCompo.js"
 import StringCalculator from './components/StringCalculator.js';
 
 function App() {
   return (
-    <div className="App"> 
-      {/* <StringCalculator /> */}
-      <NewCompo/>
+    <div className="App">
+      <StringCalculator />
     </div>
   );
 }
--- src/components/StringCalculator.js
+++ src/components/StringCalculator.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
-import { TextField, Button, Container, Typography } from '@mui/material';
+import { TextField, Button, Container, Typography, Paper, Box } from '@mui/material';
+import { Send as SendIcon } from '@mui/icons-material';
 
 const StringCalculator = () => {
-
     const [inputValue, setInputValue] = useState("")
     const [numberList, findSum] = useState();
+    const [showResult, setShowResult] = useState(false);
+    const [errorMessage, showErrorMessage] = useState("");
 
     const findNumbers = () => {
         const numbers = inputValue.match(/-?\d+(\.\d+)?/g);                      //(/\b\d+\b/g);
         if (numbers) {
+            setShowResult(true);
+            showErrorMessage("");
             const sumWithNumbers = numbers.reduce((element, current) => {
                 if (element.shouldStop) {
                     return element;
                 }
                 if (current < 0) {
                     element.shouldStop = true;
-                    element.sum = `negatives Not allowed`
+                    element.sum = 0
+                    showErrorMessage("Negative Numbers Not Allowed")
+                } else if (current > 1000) {
+                    return element;
                 } else {
                     element.sum += parseFloat(current);
                 }
@@ -24,35 +31,63 @@ const StringCalculator = () => {
 
             findSum(sumWithNumbers);
         } else {
-            findSum(0)
+            findSum({ sum: 0, shouldStop: false });
         }
     }
 
     const handleInputChange = (e) => {
-        setInputValue(e.target.value)
+        setInputValue(e.target.value);
+        setShowResult(false);
     }
 
     return (
-        <Container style={{ marginTop: '20px' }}>
-            <Typography variant="h4" gutterBottom>
-                Find Sum
-            </Typography>
-            <TextField
-                label="Enter something"
-                variant="outlined"
-                value={inputValue}
-                onChange={handleInputChange}
-                fullWidth
-                style={{ marginBottom: '20px' }}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={findNumbers}
-                disabled={!inputValue} // Disable if input is empty
-            >
-                Submit
-            </Button>
+        <Container maxWidth="sm" style={{ marginTop: '40px' }}>
+            <Paper elevation={3} style={{ padding: '20px', borderRadius: '10px' }}>
+                <Typography variant="h5" fontWeight={"bold"} align="center" gutterBottom color="success">
+                    String Calculator TDD Kata
+                </Typography>
+                <Box mb={2}>
+                    <TextField
+                        label="Enter text here"
+                        autoComplete="off"
+                        variant="outlined"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        fullWidth
+                        InputProps={{
+                            startAdornment: (
+                                <SendIcon style={{ marginRight: '8px' }} />
+                            ),
+                        }}
+                        style={{
+                            borderRadius: '10px',
+                        }}
+                    />
+                </Box>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={findNumbers}
+                    fullWidth
+                    startIcon={<SendIcon />}
+                    disabled={!inputValue} // Disable if input is empty
+                    style={{
+                        borderRadius: '10px',
+                        padding: '10px',
+                    }}
+                >
+                    Submit
+                </Button>
+                {inputValue && (
+                    <Box mt={3} textAlign="center">
+                        {errorMessage !== "" && <Typography variant="h6">
+                            <strong>{errorMessage}</strong>
+                        </Typography>}
+                        {showResult && !errorMessage && <Typography variant="h6">
+                            Output: <strong>{numberList?.sum}</strong>
+                        </Typography>}
+                    </Box>)}
+            </Paper>
         </Container>
     );
 }
